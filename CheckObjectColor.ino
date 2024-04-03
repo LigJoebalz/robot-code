39e2c66408b8ee087bbfd7199b3c7e8d1b2c87b3
BigKeaner <122934425+BigKeaner@users.noreply.github.com>
2024-04-02 22:19:16 -0400

    Create CheckObjectColor.ino

---
+++ CheckObjectColor.ino
@@ -0,0 +1,33 @@
+void checkObjectColor(){
+  digitalWrite(cTCSLED, !digitalRead(cLEDSwitch));    // turn on onboard LED if switch state is low (on position)
+  if (tcsFlag) {                                      // if colour sensor initialized
+    tcs.getRawData(&r, &g, &b, &c);                   // get raw RGBC values
+#ifdef PRINT_COLOUR            
+      Serial.printf("R: %d, G: %d, B: %d, C %d\n", r, g, b, c);
+#endif
+
+    // Set stepDir and runState based on dominant color
+    if(c>20){ 
+      if(g>b && g>r){
+        stepDir = 1;
+        runState = true;
+      }else {
+        stepDir = 0;
+        runState = true;
+      }
+    }
+
+    // Print stepDir and runState to serial monitor
+    Serial.printf("stepDir: %d, runState: %d\n", stepDir, runState);
+  }
+
+  //stepper functions
+  int speedPot = analogRead(cPotPin);                                          // read speed pot value (between 0 and 4095)
+  unsigned long stepRate = map(speedPot, 0, 4095, 500, 60000);                 // map to half period in microseconds
+  if(stepDir){
+    digitalWrite(cDirPin, 1);                                              // set direction pin
+  }
+  else{
+    digitalWrite(cDirPin, 0);                                              // set direction pin
+  }
+}
