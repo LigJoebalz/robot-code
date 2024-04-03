d4b4546815483a497155a6d284260b7f50becbe0
BigKeaner <122934425+BigKeaner@users.noreply.github.com>
2024-04-02 22:19:46 -0400

    Create ButtonISR.ino

---
+++ ButtonISR.ino
@@ -0,0 +1,10 @@
+void ARDUINO_ISR_ATTR buttonISR(void* arg) {
+  Button* s = static_cast<Button*>(arg);               // cast pointer to static structure
+
+  uint32_t pressTime = millis();                       // capture current time
+  if (pressTime - s->lastPressTime > cDebounceDelay) { // if enough time has passed to consider a valid press
+    s->numberPresses += 1;                             // increment switch press counter
+    s->pressed = true;                                 // indicate valid switch press state
+    s->lastPressTime = pressTime;                      // update time to measure next press against
+  }
+}
